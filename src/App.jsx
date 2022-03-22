0830f1f78144bcb55f724bf71b79d55e23d16d0b
Alberto <albertopimentel04@gmail.com>
2022-03-22 14:30:53 -0600

    Configuracion Inicial y Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="">
+      <h1 className="text-red-600">CRM REACT</h1>
+    </div>
+  );
+}
+
+export default App;
